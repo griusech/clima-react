d65ebfac0158de22c117b339ad930fea986995e8
Guille Riusech <Pitunetix@users.noreply.github.com>
2020-07-01 09:49:10 -0300

    Creo el Header
    Voy creando el Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Header from './components/Header'
 
 
 function App() {
   return (
-    <h1>Clima con React</h1>
+    <Fragment>
+      <Header 
+        titulo = "Clima React App"
+      />
+    </Fragment>
   );
 }
 
